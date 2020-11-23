4a549fb818bfce9034ff39808f5b9d0001df4b80
Brandon Ladson <brandonladson1999@gmail.com>
2020-11-23 11:36:41 +0100

    creazione struttura vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+// cliccando sulla chat vedo quella corrispondente;
+// con header con dati relativi a chat attiva;
+// relativa chat in elenco rimane selezionata
 
+var app = new Vue ({
+
+  el: '#app',
+  data: {
+
+
+  }
+
+})
